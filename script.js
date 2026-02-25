3e9316f7c65cec5eed1b57c9d77d4c2f45ee4697
lazy <263400119+lazykillerking@users.noreply.github.com>
2026-02-25 12:28:07 +0530

    Added terminal UI

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Boilerplate loaded");
+
+
+const input = document.getElementById("commandInput");
+const output = document.getElementById("output");
+
+const commands = {
+  help: () => `
+Available commands:
+help
+whoami
+clear
+echo
+about
+`,
+
+  whoami: () => "lkk",
+
+  about: () => "LazyKillerKing Terminal v1.0",
+
+  clear: () => {
+    output.innerHTML = "";
+    return "";
+  },
+
+  echo: (args) => args.join(" "),
+};
+
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    const value = input.value.trim();
+    const [cmd, ...args] = value.split(" ");
+
+    printLine(`lkk@terminal:~$ ${value}`);
+
+    if (commands[cmd]) {
+      const result = commands[cmd](args);
+      if (result) printLine(result);
+    } else if (value !== "") {
+      printLine(`Command not found: ${cmd}`);
+    }
+
+    input.value = "";
+    window.scrollTo(0, document.body.scrollHeight);
+  }
 });
+
+function printLine(text) {
+  const div = document.createElement("div");
+  div.innerHTML = text;
+  output.appendChild(div);
+}
