b27f51ec51f3f9c11cf5c970ac589b8f0c69b22a
lazy <263400119+lazykillerking@users.noreply.github.com>
2026-02-26 23:05:06 +0530

    updated to v4.6.6

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let draftInput = "";
 
 const user = "lkk";
 const DEFAULT_ROOT = "terminal_fs";
-const TERMINAL_VERSION = "4.6.5";
+const TERMINAL_VERSION = "4.6.6";
 
 // Per-folder mount password hashes (SHA-256) for root switching with `mount`.
 const MOUNT_PASSWORD_HASHES = {
@@ -1187,23 +1187,13 @@ function registerCommands() {
     async run() {
       setTimeout(() => {
         window.close();
-        // If close fails (HTTP/HTTPS restriction), try navigating to the browser home page
+        // if close is blocked, redirect straight to Google
         if (!window.closed) {
-          // common home page URIs; browser will use whichever it supports
-          const homeCandidates = ['about:home', 'about:newtab', 'chrome://newtab'];
-          for (const uri of homeCandidates) {
-            try {
-              window.location = uri;
-              return;
-            } catch (_) {}
-          }
-          // if the browser won't accept a home URI, try a public page
           try {
             window.location = 'https://www.google.com/';
-            return;
-          } catch (_) {}
-          // all else fails, fall back to blank
-          window.location = 'about:blank';
+          } catch (_) {
+            window.location = 'about:blank';
+          }
         }
       }, 500);
       return { text: "Goodbye!" };
